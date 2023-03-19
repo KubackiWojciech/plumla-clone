ff3fd9e2ef339bd63ea4b2676a06a12c3e26d09b
Kubacki Wojciech <kubackiwo@gmail.com>
2023-03-19 17:07:27 +0100

    added gallery part

--- src/parts/gallery/Gallery.tsx
+++ src/parts/gallery/Gallery.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
 
+import spaceImg from '../../../static/space.webp'
+import space2Img from '../../../static/space2.jpg'
+
+import './gallery.sass'
+
 export default function Gallery() {
   return (
-    <div className='main-part'>Gallery</div>
+    <div className='main-part' id='gallery-container'>
+      <img src={spaceImg} />
+      <img src={space2Img} />
+    </div>
   )
 }
